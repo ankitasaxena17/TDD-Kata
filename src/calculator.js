a8c3bf6eb81ef5a0d3e855f4ab5279e653d17641
Ankita Saxena <ankitasaxena@Ankitas-MacBook-Air.local>
2025-02-08 22:40:02 +0530

    handled comma seperated values case

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,11 @@
 export function add(numbers) {
+    // case: empty string
     if(numbers === "") return 0;
 
+    // case: single value
     if (!isNaN(numbers)) return Number(numbers);
 
+    // case: comma seperated values
+    const numArray = numbers.split(',').map(num => Number(num.trim()));
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
