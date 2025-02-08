7bfbb3d8c7ba6e3f4d29a1d81c3f6043155126df
Ankita Saxena <ankitasaxena@Ankitas-MacBook-Air.local>
2025-02-08 23:48:15 +0530

    handled custom delimiter case

--- src/calculator.js
+++ src/calculator.js
@@ -1,11 +1,25 @@
-export function add(numbers) {
+const checkAndReturnCustomDelimitersAndInput = (input) => {
+    let customDelimiter = '';
+    if (input.startsWith('//')) {
+        const parts = input.split('\n');
+        customDelimiter = parts[0].substring(2);
+        return {
+            customDelimiter, delimeterSeperatedInput: parts[1]
+        }
+    }
+    return {customDelimiter, delimeterSeperatedInput: input}
+}
+
+export function add(input) {
     // case: empty string
-    if(numbers === "") return 0;
+    if(input === "") return 0;
 
     // case: single value
-    if (!isNaN(numbers)) return Number(numbers);
+    if (!isNaN(input)) return Number(input);
 
-    // case: comma seperated values and new line separator
-    const numArray = numbers.split(/[\n,]/).map(num => Number(num.trim()));
+    // case: comma seperated values, new line separator and custom delimiter
+    const {customDelimiter, delimeterSeperatedInput} =  checkAndReturnCustomDelimitersAndInput(input)
+    const delimiters = [',', '\n', customDelimiter];
+    const numArray = delimeterSeperatedInput.split(new RegExp(`[${delimiters.join('')}]`)).map(num => Number(num.trim()));
     return numArray.reduce((sum, num) => sum + num, 0);
 }
